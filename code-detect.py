b339e5663cdb797da2e5da3724680065dfdec58c
Vivek Agarwal <vivek17.agarwal@gmail.com>
2015-11-08 16:16:07 +0530

    introduced modularity, directory search for files

--- code-detect.py
+++ code-detect.py
@@ -1,8 +1,12 @@
 import ast
 import re
+import sys
+import os
 
 l1 = []
 l2 = []
+d = {}
+
 def prettyprint(node, no_of_space, p, i):
     #print(" "*no_of_space,"Node: ", node)
     #if(no_of_space != 0):
@@ -22,46 +26,49 @@ def prettyprint(node, no_of_space, p, i):
     		l2.append(p)
 
 
-f_complete = open("merge-complete.py")
-f_incomplete = open("merge-incomplete.py")
-
-str = ""
-for line in f_complete:
-	str += line
-
-t = ast.parse(str)
-#print("Tree 1 : \n")
-prettyprint(t, 0, "",1)
-
-#for i, w in enumerate(l1):
-#	print(i,": ",w)
-
-#print("Tree 1 : ")
-str = ""
-for line in f_incomplete:
-	str += line
-
-t = ast.parse(str)
-#print("Tree 2 : \n\n")
-prettyprint(t, 0, "",2)
-
-#print(str)
-print("Tree 2 : \n")
-#for i, w in enumerate(l2):
-#	print(i,": ",w)
+def extract_file_code(f):
+	str = ""
+	for line in f:
+		str += line
+	return str
 
 
-match = 0
-mx = 0
-for w2 in l2:
-	w2.strip()
-	for w1 in l1:
-		w1.strip()
-		if(w2 in w1 and w2 != ' ') :
-			match += 1
-			#l1.remove(w1)
-			break
+def build_incomplete_code_ast(file_name):
+	f_incomplete = open(file_name)
+	str_incomplete = ""
+	str_incomplete = extract_file_code(f_incomplete)
+	tree_incomplete = ast.parse(str_incomplete)
+	prettyprint(tree_incomplete, 0, "", 2)
 
-print("Total paths in incomplete code : ", len(l2))
-print("Matched Paths : ",match)
-print("Percentage : ", float(match) * 100 / len(l2) )
+def iterate_directory(dir_name):
+	for filename in os.listdir(dir_name):
+		l1[:] = []
+		f_complete = open(dir_name+'/'+filename)					#change it to backslash for unix based systems
+		str_complete = extract_file_code(f_complete)
+		tree_complete = ast.parse(str_complete)
+		prettyprint(tree_complete, 0, "", 1)
+		match_paths(filename)
+
+
+
+def match_paths(filename):
+	match = 0
+	for w2 in l2:
+		w2.strip()
+		for w1 in l1:
+			w1.strip()
+			if(w2 in w1 and w2 != ' ') :
+				match += 1
+				#l1.remove(w1)
+				break
+
+	t = (float(match) * 100 ) / len(l2)
+	d[filename] = t
+
+if __name__ == "__main__":
+	build_incomplete_code_ast(sys.argv[1])
+	iterate_directory(sys.argv[2])
+	print("Current File : ", sys.argv[1])
+	print("Filename : \tMatching Percentage ")
+	for filename, match in d.items():
+		print(filename," : ", "{0:.2f}".format(round(match,2)))
