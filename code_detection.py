7b3a96e4877a0cabfb2b8d0d3fe9fba75570dae1
Prashant Baisla <prashantbaisla@gmail.com>
2015-11-20 18:44:01 +0530

    Sort output panel results

--- code_detection.py
+++ code_detection.py
@@ -2,6 +2,7 @@ import sublime, sublime_plugin, os
 from .lib.CodeComparer import CodeComparer
 
 class CodeDetectionCommand(sublime_plugin.TextCommand):
+
   def run(self, edit):
     self.view.window().show_input_panel("Search directory", "", self.compare, None, None)
 
@@ -12,13 +13,20 @@ class CodeDetectionCommand(sublime_plugin.TextCommand):
 
     matches = comparer.compare(code_body)
     match_list = []
-    self.files = []
 
     for filename, match_percentage in matches.items():
-      match_list.append(filename + " {0:.2f}".format(match_percentage))
+      match_list.append((filename, match_percentage))
+
+    match_list.sort(key=lambda m: m[1], reverse=True)
+
+    self.files = []
+    panel_list = []
+
+    for filename, match_percentage in match_list:
+      panel_list.append(filename + " {0:.2f}".format(match_percentage))
       self.files.append(filename)
 
-    self.view.window().show_quick_panel(match_list, self.show_file)
+    self.view.window().show_quick_panel(panel_list, self.show_file)
 
 
   def show_file(self, index):
